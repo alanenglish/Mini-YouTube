f940d3ea7d9390325d36a3976dd4e487fd0e0ef6
Alan English <aenglish@manorhillcapital.com>
2017-09-30 21:56:24 -0500

    change component to class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react'; // pull off property component as variable called component
 
-const SearchBar = () => {
-  return <input />;                  // React.createElement
-};
+class SearchBar extends Component {
+  render() {          // every class must have a render function
+    return <input />;            // must return some JSX
+  }
+}                     // React.createElement
 
 // any file that imports SearchBar will get the component
 export default SearchBar;

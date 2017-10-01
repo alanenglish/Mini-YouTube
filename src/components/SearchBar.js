622744ae546930e37f2bc11a280fb5fdfdb139ba
Alan English <aenglish@manorhillcapital.com>
2017-09-30 22:06:21 -0500

    create on change property and pass function on change of input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'; // pull off property component as vari
 
 class SearchBar extends Component {
   render() {          // every class must have a render function
-    return <input />;            // must return some JSX
+    // whenever writing JSX and using Javascript variables, wrap in curly braces
+    // onChange is now passed as a prop
+    return <input onChange={event => console.log(event.target.value)} />;  // must return some JSX // React.createElement
   }
-}                     // React.createElement
+
+  // event handler defined as method on the class
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 // any file that imports SearchBar will get the component
 export default SearchBar;
